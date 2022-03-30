2359a1c411e7d7aaee8ae87fb299ad7fd2e27d1f
Sreerag  C <sreerag.c@ayatacommerce.com>
2022-03-30 16:16:37 +0530

    Initial commit

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String arg[])
+    {
+        System.out.println("welcome sreerag");
+    }
+}
+
---
+++ src/com/ayata/basics/Welcome.java
@@ -0,0 +1,8 @@
+package com.ayata.basics;
+
+public class Welcome {
+    public static void main(String args[])
+    {
+        System.out.println("welcome");
+    }
+}
